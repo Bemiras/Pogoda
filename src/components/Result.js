c875e00323f794f38edd97322d3bea07b0be6b28
Bemiras <bartektojestemja12@gmail.com>
2022-01-26 19:33:42 +0100

    New view

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,25 +3,56 @@ import './Result.css'
 
 const Result = props => {
 
-    const {date, city, sunrise, sunset, temp, tempMax, tempFeel, pressure, visibility, humidity, wind, err} = props.weather;
+    const {city, sunrise, sunset, temp, tempFeel, pressure, visibility, humidity, wind, err} = props.weather;
     let content = null;
 
     if(!err && city) {
         const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
         const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
+
         content =(
             <React.Fragment>
-                <h3><em>{city}</em></h3>
-                <h4>Aktualny czas: {date}</h4>
-                <h4>Temperatura: {temp} &#176;C</h4>
-                <h4>Max temperatura: {tempMax} &#176;C</h4>
-                <h4>Odczuwalna temperatura: {tempFeel} &#176;C</h4>
-                <h4>Widoczność: {visibility}m</h4>
-                <h4>Wilgotność powietrza: {humidity} %</h4>
-                <h4>Wschód słońca: {sunriseTime} </h4>
-                <h4>Zachód słońca: {sunsetTime} </h4>
-                <h4>Ciśnienie atmosferyczne: {pressure} hPa</h4>
-                <h4>Siła wiatru: {wind} m/s</h4>
+                <div className="data">
+                    <div>
+                        <h3><em>{city}</em></h3>
+                        <table cellSpacing='20'  id="tableresult">
+                            <tbody>
+                            <tr>
+                                <th className="title"><i className="material-icons">thermostat</i> Temperatura</th>
+                                <th className="title"><i className="material-icons">thermostat</i> Odczuwalna</th>
+                            </tr>
+                            <tr>
+                                <td>{temp}&#176;C</td>
+                                <td>{tempFeel}&#176;C</td>
+                            </tr>
+                            <tr>
+                                <td className="title"><i className="material-icons">visibility</i> Widoczność</td>
+                                <td className="title"><i className="material-icons">air</i>Siła wiatru</td>
+                            </tr>
+                            <tr>
+                                <td>{visibility}m</td>
+                                <td>{wind} m/s</td>
+                            </tr>
+                            <tr>
+                                <td className="title"><i className="material-icons">wb_sunny</i> Wschód</td>
+                                <td className="title"><i className="material-icons">wb_sunny</i> Zachód</td>
+                            </tr>
+                            <tr>
+                                <td>{sunriseTime}</td>
+                                <td>{sunsetTime} </td>
+                            </tr>
+                            <tr>
+                                <td className="title">Ciśnienie</td>
+                                <td className="title">Wilgotność</td>
+                            </tr>
+                            <tr>
+                                <td>{pressure} hPa</td>
+                                <td>{humidity} %</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
